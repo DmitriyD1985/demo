cf31505f061139cdbd0a2d18fea7ceb2962a1b97
Dmitriy Demidov <40824595+DmitriyD1985@users.noreply.github.com>
2019-04-30 13:03:18 +0300

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,16 +1,31 @@
 class Product {
-    public String name;
-    public int weigth;
-    public int cost;
-    private double indexPolzi;
+    public int getCost;
+    private String name;
+    private int weigth;
+    private int cost;
 
     public Product(String name, int weigth, int cost){
         this.name = name;
         this.weigth = weigth;
         this.cost = cost;
-        indexPolzi = this.cost/this.weigth;
     }
-    public double getIndex(){
-        return indexPolzi;
+
+    public void setName(String name){
+        this.name = name;}
+    public void setWeigth(int weigth){
+        this.weigth = weigth;
+    }
+    public void setCost(int cost){
+        this.cost = cost;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public int getWeigth(){
+        return weigth;
+    }
+    public int getCost(){
+        return cost;
     }
 }
