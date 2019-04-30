2891436b23a5439be02bde2e9ed29332b81e9f5e
Dmitriy Demidov <40824595+DmitriyD1985@users.noreply.github.com>
2019-04-30 13:02:49 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,62 +8,78 @@ import java.util.Comparator;
 import java.lang.Comparable;
 
 public class Main {
-    public static ArrayList<Product> Ucomplekt(ArrayList<Product> list, int value) {
-        ArrayList<Product> zagruz = new ArrayList<>();
-        for (int i = list.size() - 1; i > 0; i--) {
-            for (int j = 0; j < i; j++) {
-                if (list.get(j).getIndex() > list.get(j + 1).getIndex()) {
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int capacity = Integer.parseInt(reader.readLine());
+        Truck truck = new Truck(capacity);
+
+        ArrayList<Product> listProduct = new ArrayList<>();
+        String str = reader.readLine();
+        String[] subStr;
+        String[] subStr2;
+        subStr = str.split(" ");
+        String name;
+        int weigth;
+        int cost;
+        for (int i = 0; i < subStr.length; i++)
+        {
+            String inputProduct = subStr[i];
+            subStr2 = inputProduct.split("/");
+            name = subStr2[0];
+            weigth = Integer.parseInt(subStr2[1]);
+            cost = Integer.parseInt(subStr2[2]);
+            listProduct.add(new Product(name, weigth, cost));
+        }
+
+        ArrayList<Product> cargo = new ArrayList<>();
+        cargo = manningСargo(listProduct, truck);
+        String names = "";
+        int priceAll = 0;
+        for(int i =0; i<cargo.size(); i++)
+        {
+            names += cargo.get(i).getName()+" ";
+            priceAll +=cargo.get(i).getCost();
+        }
+
+        System.out.println(names+priceAll);
+
+
+    }
+
+    public static ArrayList<Product> manningСargo(ArrayList<Product> list, Truck truck)
+    {
+        ArrayList<Product> value = new ArrayList<>();
+        for (int i = list.size() - 1; i > 0; i--)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (benefitIndex(list.get(j),truck)> benefitIndex(list.get(j+1),truck)) {
                     Product tmp = list.get(j);
                     list.set(j, list.get(j + 1));
                     list.set(j + 1, tmp);
                 }
             }
-
-
         }
-        int zagrujeno =0;
-        for (int i = list.size()-1; i>= 0;i--) {
-            if ((zagrujeno + list.get(i).weigth > 50)) {
-                break;
-            } else {
-                zagruz.add(list.get(i));
-                zagrujeno += list.get(i).weigth;
-            }
-        }
-        return zagruz;
-    }
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-        int value = Integer.parseInt(reader.readLine());
-        ArrayList<Product> listProd = new ArrayList<>();
-        String str = reader.readLine();
-        String name;
-        int weigth;
-        int cost;
-        String[] subStr;
-        String[] subStr2;
-        subStr = str.split(" ");
-
-        for (int i = 0; i < subStr.length; i++) {
-            String vvodTowar = subStr[i];
-            subStr2 = vvodTowar.split("/");
-            name = subStr2[0];
-            weigth = Integer.parseInt(subStr2[1]);
-            cost = Integer.parseInt(subStr2[2]);
-            listProd.add(new Product(name, weigth, cost));
-        }
-        ArrayList<Product> gruz = new ArrayList<>();
-        gruz = Ucomplekt(listProd, value);
-        String names = "";
-        int price = 0;
-        for(int i =0; i<gruz.size(); i++)
+        int cargoIn =0;
+        for (int i = list.size()-1; i>= 0;)
         {
-            names += gruz.get(i).name+" ";
-            price +=gruz.get(i).cost;
+            if((list.get(i).getWeigth()+cargoIn)<=truck.getCapacity()){cargoIn += list.get(i).getWeigth(); value.add(list.get(i));i--;}
+            else i--;
+//            if ((cargoIn + list.get(i).getWeigth() > truck.getCapacity()))
+//            { break
+//            } else {
+//                value.add(list.get(i));
+//                cargoIn += list.get(i).getWeigth();
+//            }
         }
-
-        System.out.println(names+price);
-
+        return value;
     }
+
+    public static double benefitIndex (Product product, Truck truck){
+        double index = (product.getWeigth()/product.getCost())*truck.getCapacity();
+        return index;
+    }
+
 }
