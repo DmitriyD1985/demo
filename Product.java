0805a5059ac5d6d9813155347c7686da83a98c37
Dmitriy Demidov <40824595+DmitriyD1985@users.noreply.github.com>
2019-04-30 13:41:24 +0300

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,31 +1,39 @@
-class Product {
+class Product 
+{
     public int getCost;
     private String name;
     private int weigth;
     private int cost;
 
-    public Product(String name, int weigth, int cost){
+    public Product(String name, int weigth, int cost)
+    {
         this.name = name;
         this.weigth = weigth;
         this.cost = cost;
     }
 
-    public void setName(String name){
+    public void setName(String name)
+    {
         this.name = name;}
-    public void setWeigth(int weigth){
+    public void setWeigth(int weigth)
+    {
         this.weigth = weigth;
     }
-    public void setCost(int cost){
+    public void setCost(int cost)
+    {
         this.cost = cost;
     }
 
-    public String getName(){
+    public String getName()
+    {
         return name;
     }
-    public int getWeigth(){
+    public int getWeigth()
+    {
         return weigth;
     }
-    public int getCost(){
+    public int getCost()
+    {
         return cost;
     }
 }
